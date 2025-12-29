159e920d52ce92ab64a1ab0588b4842d680fb571
nageetaw <nigeetawadhwani@gmail.com>
2025-12-29 20:57:22 +0100

    added router to main

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.auth import router as auth_router
 from app.api.health import router as health_router
+from app.api.audio import router as audio_router
 from app.core.config import settings
 from typing import AsyncGenerator
 from app.db.session import sessionmanager
@@ -52,6 +53,7 @@ app.add_middleware(
 # Include routers
 app.include_router(health_router, tags=["system"])
 app.include_router(auth_router, prefix="/auth", tags=["authentication"])
+app.include_router(audio_router, prefix="/audio", tags=["audio"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
